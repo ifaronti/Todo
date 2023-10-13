9f1970329f54c90ec5398dabb371fdab6d911743
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-10-13 19:15:16 +0100

    Add files via upload

---
+++ NavBar.js
@@ -0,0 +1,6 @@
+export default function NavBar(){
+return ( <nav className='navBar'>
+            <img className="nav_image" src={`${process.env.PUBLIC_URL}/asset/images/light.jpg`} alt="/" />
+        </nav>
+  )
+}
---
+++ Todos.js
@@ -0,0 +1,6 @@
+export default function Todo(props){
+return( <fieldset className="todoDiv" >
+            {props.todo}
+        </fieldset>
+  )
+}
