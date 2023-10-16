7dab0018518eb881e8c1beeab3df864a33c67599
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-10-16 18:25:49 +0100

    Delete NavBar.js

--- NavBar.js
+++
@@ -1,6 +0,0 @@
-export default function NavBar(){
-return ( <nav className='navBar'>
-            <img className="nav_image" src={`${process.env.PUBLIC_URL}/asset/images/light.jpg`} alt="/" />
-        </nav>
-  )
-}
